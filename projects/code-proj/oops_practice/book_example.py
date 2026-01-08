73e30708272252450b81661b83eebb84652a7b63
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-01-08 17:53:31 +0530

    Update book_example.py

--- projects/code-proj/oops_practice/book_example.py
+++ projects/code-proj/oops_practice/book_example.py
@@ -1,4 +1,4 @@
-class Book:
+class book:
     def __init__(self, name, author, copies):
         self.name = name
         self.author = author
@@ -7,13 +7,23 @@ class Book:
     def __repr__(self):
         return repr((self.name, self.author, self.copies))
 
-book1 = Book("Mastering Spring 5.0", "Author1", "14")
-book2 = Book("Mastering Python", "Author2", "244")
+    def increase_copies(self, how_many):
+        self.copies += how_many
+
+    def decrease_copies(self, how_many):
+        self.copies -= how_many
+
+
+book1 = book("Mastering Spring 5.0", "Author1", 14)
+book2 = book("Mastering Python", "Author2", 244)
+
+book1.increase_copies(5)
+book2.decrease_copies(12)
 
 print(book1)
 print(book2)
 
 
 # ---output--
-# ('Mastering Spring 5.0', 'Author1', '14')
-# ('Mastering Python', 'Author2', '244')
+# ('Mastering Spring 5.0', 'Author1', 19)
+# ('Mastering Python', 'Author2', 232)
