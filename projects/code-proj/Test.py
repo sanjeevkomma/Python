b37a7466bc38a92cea0216d574db9f6aef093379
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-01-08 13:53:11 +0530

    Update Test.py

--- projects/code-proj/Test.py
+++ projects/code-proj/Test.py
@@ -12,3 +12,19 @@ print(type(country))
 print(country.lower())
 print(country.capitalize())
 print("india".capitalize())
+
+
+x = 5
+if x > 3 and x < 6:
+    print(f"{x} is greater than 3")
+elif x < 6:
+    print(f"{x} is less than 6")
+
+number = 5
+if(number%2 == 0):
+    isEven = True
+else:
+    isEven = False
+
+isEven = True if number%2 == 0 else False
+print(isEven)
