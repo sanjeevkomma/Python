bd8ac417fafee60b779bd77689f75161d06434df
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-01-08 14:32:36 +0530

    Create for_loop_example.py

---
+++ projects/code-proj/for_loop_example.py
@@ -0,0 +1,19 @@
+for i in range(1,10):
+    print(i)
+    print("Done")
+
+
+for i in range(1,10):
+    print(i*i)
+
+print("Test")
+
+
+for ch in "Hello World":
+    print(ch)
+
+for word in "Hello World".split():
+    print(word)
+
+for item in (3,8,9):
+    print(item)
