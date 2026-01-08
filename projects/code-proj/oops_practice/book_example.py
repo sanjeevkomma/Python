2d33640a5fdf33bfc79db2933dbf825056d70cc4
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-01-08 16:08:18 +0530

    Update book_example.py

--- projects/code-proj/oops_practice/book_example.py
+++ projects/code-proj/oops_practice/book_example.py
@@ -1,18 +1,19 @@
 class Book:
-    def __init__(self, name, author):
+    def __init__(self, name, author, copies):
         self.name = name
         self.author = author
+        self.copies = copies
 
     def __repr__(self):
-        return repr(self.name + ' ' + self.author)
+        return repr((self.name, self.author, self.copies))
 
-book1 = Book("Mastering Spring 5.0", "Author1");
-book2 = Book("Mastering Python", "Author2");
+book1 = Book("Mastering Spring 5.0", "Author1", "14")
+book2 = Book("Mastering Python", "Author2", "244")
 
 print(book1)
 print(book2)
 
 
 # ---output--
-# 'Mastering Spring 5.0 Author1'
-# 'Mastering Python Author2'
+# ('Mastering Spring 5.0', 'Author1', '14')
+# ('Mastering Python', 'Author2', '244')
