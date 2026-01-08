05a7b3a6a4495f765a1bd56c7478c6f35fd0806d
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-01-08 15:05:19 +0530

    Create repeated_question.py

---
+++ projects/code-proj/repeated_question.py
@@ -0,0 +1,7 @@
+# number = int(input('Enter a number: '))
+# print(f'cube is {number ** 3}')
+
+number1 = 0
+while number1 >= 0:
+    number1 = int(input('Enter a number: '))
+    print(f'cube is {number1 ** 3}')
