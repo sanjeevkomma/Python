aa03a83ee05b59768bbf78d261bc66fe24281707
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-01-08 15:55:08 +0530

    Update book_example.py

--- projects/code-proj/oops_practice/book_example.py
+++ projects/code-proj/oops_practice/book_example.py
@@ -1,8 +1,17 @@
 class Book:
-    pass
+    def __init__(self, name):
+        self.name = name
 
-book1 = Book();
-book2 = Book();
+    def __repr__(self):
+        return repr(self.name)
+
+book1 = Book("Mastering Spring 5.0");
+book2 = Book("Mastering Python");
 
 print(book1)
 print(book2)
+
+
+# ---output--
+# 'Mastering Spring 5.0'
+# 'Mastering Python'
