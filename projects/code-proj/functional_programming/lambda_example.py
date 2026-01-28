7a2b2d45eb33d424fc5b1d1edf03669de05a4a41
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-01-28 18:56:21 +0530

    Update lambda_example.py

--- projects/code-proj/functional_programming/lambda_example.py
+++ projects/code-proj/functional_programming/lambda_example.py
@@ -7,3 +7,9 @@ def do_this_and_print(func, data):
 do_this_and_print(multiply_by_3,125) # 375
 
 do_this_and_print(lambda data:data * 5, 8) # 40
+
+do_this_and_print(lambda data:data * data , 55) # 3025
+
+do_this_and_print(lambda data: data ** 3 , 6) # 216
+
+do_this_and_print(lambda data: len(data), 'Test') # 4
