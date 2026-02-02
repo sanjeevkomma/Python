71484439a18b4ca27988ffb7146a2eed4b35ae3d
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-02-02 12:06:36 +0530

    Create modules_example.py

---
+++ projects/code-proj/built-in_modules/modules_example.py
@@ -0,0 +1,27 @@
+import math # math is built-in module
+
+print(math.floor(4.5)) # 4
+print(math.ceil(4.5)) # 5
+
+
+#help(math.floor)
+# Help on built-in function floor in module math:
+#
+# floor(x, /)
+#     Return the floor of x as an Integral.
+#
+#     This is the largest integer <= x.
+
+print('===========')
+#help(math)
+
+from math import *
+print(floor(5)) # 5
+print(ceil(5)) # 5
+print(gcd(34,56)) # 2
+
+#print(help(floor))
+#print(help(gcd))
+
+from math import gcd
+print(gcd(33,45)) # 3
