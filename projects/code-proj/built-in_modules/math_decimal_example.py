4b03b017eaa79528f67998008f580abd329fd790
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-02-02 12:22:33 +0530

    Create math_decimal_example.py

---
+++ projects/code-proj/built-in_modules/math_decimal_example.py
@@ -0,0 +1,11 @@
+import math # same as "from math import *"
+from decimal import Decimal
+
+value1 = Decimal('4.5')
+value2 = Decimal('3.2')
+
+print(value1-value2) # 1.3
+
+print(math.pi) # 3.141592653589793
+print(math.e) # 2.718281828459045
+# print(help(math.factorial))
