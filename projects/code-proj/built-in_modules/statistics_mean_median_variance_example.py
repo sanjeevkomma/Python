1a0e55ce9ad89188e846315f52386b56c09aaf78
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-02-02 12:28:46 +0530

    Create statistics_mean_median_variance_example.py

---
+++ projects/code-proj/built-in_modules/statistics_mean_median_variance_example.py
@@ -0,0 +1,8 @@
+import statistics
+
+marks = [1,6,9,23,2,7]
+print(statistics.mean(marks)) # 8
+print(statistics.median(marks)) # 6.5
+print(statistics.median_high(marks)) # 7
+print(statistics.median_low(marks)) # 6
+print(statistics.variance(marks)) # 63.2
