50c9d3a8d8ce36d268ce638cff4ad125dba0fe04
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-02-03 21:34:08 +0530

    Create return_multi_values_using_Tuple.py

---
+++ projects/code-proj/python_tips/return_multi_values_using_Tuple.py
@@ -0,0 +1,8 @@
+def some_function():
+    return 23,"some string", 4.5
+
+tuple_example = some_function()
+
+print(tuple_example) # (23, 'some string', 4.5)
+print(tuple_example[0]) # 23
+print(tuple_example[1]) # some string
