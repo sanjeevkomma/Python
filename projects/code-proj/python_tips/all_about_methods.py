f85c4fbae2c86270d355cde5f06183605ccb3d89
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-02-03 13:12:51 +0530

    Update all_about_methods.py

--- projects/code-proj/python_tips/all_about_methods.py
+++ projects/code-proj/python_tips/all_about_methods.py
@@ -1,6 +1,3 @@
-from src.python_tips.enum_example import Currency
-
-
 def example_method(mandatory_parameter, default_parameter="Default",
                    *args, **kwargs):   # args is Tuple, kwargs(keyword arguments) is Dictionary
     print(f"""
@@ -26,3 +23,33 @@ print('=========')
 example_method(25, "String 1","String 2","String 3","String 4","String 5")
 example_method(25,"String 1", key1='a', key2='b')
 example_method(key1='a', key2='b',mandatory_parameter=33, default_parameter="String 1")
+print('=============================================')
+example_list = [10,20,30,40,50,60]
+example_method(example_list[0],example_list[1],example_list[2])
+# mandatory_parameter: 10 <class 'int'>
+#          default_parameter: 20 <class 'int'>
+#          args: (30,) <class 'tuple'>
+#          kwargs: {} <class 'dict'>
+example_method(*example_list) # argument unpacking
+# mandatory_parameter: 10 <class 'int'>
+#          default_parameter: 20 <class 'int'>
+#          args: (30, 40, 50, 60) <class 'tuple'>
+#          kwargs: {} <class 'dict'>
+example_dict = {'a':'1','b':'2'}
+example_method(*example_list, **example_dict)
+# mandatory_parameter: 10 <class 'int'>
+#          default_parameter: 20 <class 'int'>
+#          args: (30, 40, 50, 60) <class 'tuple'>
+#          kwargs: {'a': '1', 'b': '2'} <class 'dict'>
+
+
+
+
+
+
+
+
+
+
+
+
