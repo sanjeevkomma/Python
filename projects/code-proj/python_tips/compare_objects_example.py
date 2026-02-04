33f5e01bdcab6cf6ffaaeffcc2ef2fea342dc574
Sanjeev Komma <sanjeevkomma@gmail.com>
2026-02-04 21:39:40 +0530

    Update compare_objects_example.py

--- projects/code-proj/python_tips/compare_objects_example.py
+++ projects/code-proj/python_tips/compare_objects_example.py
@@ -1,3 +1,44 @@
+from functools import total_ordering
+
 print((1,1) == (1,1)) # True
 print((1,1) > (1,1)) # False
 print((4,1) > (3,1)) # True
+
+
+i = 1
+j = 2
+# print(i+j) # Here i & j are object. In python, everything is object, there is no primitive type
+
+@total_ordering
+class Money:
+    def __init__(self, currency, amount):
+        self.currency = currency
+        self.amount = amount
+
+    def __add__(self, other):
+        return Money(self.currency, self.amount + other.amount)
+    def __sub__(self, other):
+        return Money(self.currency, self.amount - other.amount)
+
+    def __eq__(self, other):
+        return (self.currency, self.amount) == (other.currency, other.amount)
+    # def __eq__(self, other):
+    #     return (self.currency, self.amount) < (other.currency, other.amount)
+
+    def __gt__(self, other):
+        return (self.currency, self.amount) > (other.currency, other.amount)
+
+    def __repr__(self):
+        return repr((self.currency, self.amount))
+
+amount1 = Money('USD', 100)
+amount2 = Money('USD', 300)
+print(amount1 + amount2) # ('USD', 400)
+print(amount1 - amount2) # ('USD', -100)
+print(amount1 == amount2) # False
+print(amount1 != amount2) # True
+
+print(amount1 > amount2) # False
+print(amount1 >= amount2) # False
+print(amount1 < amount2) # True
+print(amount1 <= amount2) # True
